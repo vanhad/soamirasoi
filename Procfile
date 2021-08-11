d33bde10cdb466dc3940a44937b513e0b5cb50d1
Anhad <vanhad72@gmail.com>
2021-08-11 22:59:00 +0530

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn SoamiRasoiProj.wsgi --log-file -
--- SoamiRasoiProj/settings.py
+++ SoamiRasoiProj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b!(#m2c7d4p8_ua_v$jqu2y4t#p(!f^n0qn*j!^y4%s5g357tw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
