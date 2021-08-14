4b7c2011f05f52adc5db4184f96669f76cc19d89
Anhad <vanhad72@gmail.com>
2021-08-14 21:59:07 +0530

    DB Updated to pgsql

--- SoamiRasoiProj/settings.py
+++ SoamiRasoiProj/settings.py
@@ -85,6 +85,12 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
