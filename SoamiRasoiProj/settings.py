eefe6ea8229c59e875acf9e4e04002743f9ea93b
Anhad <vanhad72@gmail.com>
2021-08-15 21:56:05 +0530

    static files added apps 2

--- SoamiRasoiProj/settings.py
+++ SoamiRasoiProj/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
