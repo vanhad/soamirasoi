8b26bd8e4f72986579a2826a5686689b639ba0d5
Anhad <vanhad72@gmail.com>
2021-08-15 15:13:28 +0530

    com

--- SoamiRasoiProj/settings.py
+++ SoamiRasoiProj/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'SoamiRasoiProj/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
