05799743fe96665ddcb6f362e8171c7d1ce1b09e
Anhad <vanhad72@gmail.com>
2021-08-16 10:11:16 +0530

    pdate wsgi.py

--- SoamiRasoiProj/wsgi.py
+++ SoamiRasoiProj/wsgi.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+from django.conf import settings
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SoamiRasoiProj.settings')
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise.django import WhiteNoise
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application,root=settings.STATIC_ROOT)
+application.add_files(settings.STATIC_ROOT, prefix='more-files/')
