052986aa96b15c4465d4bdb3b0b2f825cd965cce
Anhad <vanhad72@gmail.com>
2021-08-16 13:26:18 +0530

    Update settings.py

--- SoamiRasoiProj/settings.py
+++ SoamiRasoiProj/settings.py
@@ -146,8 +146,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/static/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
 
 
 # STATIC_URL = '/static/'
