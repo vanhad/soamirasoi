ad3eebd2eb140816e18c50aa414aecbabe69d051
Anhad <vanhad72@gmail.com>
2021-08-16 14:22:50 +0530

    Revert "Update settings.py"
    This reverts commit 6db1c5cee8e37ee7bff1a958f7e8c7d3361833a8.

--- SoamiRasoiProj/settings.py
+++ SoamiRasoiProj/settings.py
@@ -142,10 +142,9 @@ USE_TZ = True
 # STATIC_URL = STATIC_HOST + '/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets')
+    os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_TMP = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
