e35691ca128045ef8fe8a3cdcb4f4f0147a5abe6
Anhad <vanhad72@gmail.com>
2021-08-16 14:45:20 +0530

    ghf

--- SoamiRasoiProj/settings.py
+++ SoamiRasoiProj/settings.py
@@ -142,16 +142,15 @@ USE_TZ = True
 # STATIC_URL = STATIC_HOST + '/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets')
+    os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_TMP = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-MEDIA_URL = '/static/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STATIC_URL = '/static/'
+
 
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, "static"),
