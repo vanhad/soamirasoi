36e450aab7f40b17b01c973ff914018146aa58d3
Anhad <vanhad72@gmail.com>
2021-08-17 16:22:06 +0530

    Update settings.py

--- SoamiRasoiProj/settings.py
+++ SoamiRasoiProj/settings.py
@@ -162,6 +162,7 @@ AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
