cd7c777c1829f832cc3224572f0a69df2a3c6226
Anhad <vanhad72@gmail.com>
2021-08-17 15:00:33 +0530

    Update settings.py

--- SoamiRasoiProj/settings.py
+++ SoamiRasoiProj/settings.py
@@ -162,6 +162,10 @@ AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
