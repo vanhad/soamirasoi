38d45ff4ca63db1abf6345f1dfe27a82cbec471f
vanhad <78998518+vanhad@users.noreply.github.com>
2022-08-08 15:34:24 +0530

    Update settings.py

--- SoamiRasoiProj/settings.py
+++ SoamiRasoiProj/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'assets')
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # MEDIA_URL = 'http://s3.amazonaws.com/soamirasoi-assets'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'soamirasoi-assets')
